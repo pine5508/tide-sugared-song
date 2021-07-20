3cc4515263406951ce1eaf6a5817be1f7108d3e4
Glitch (cooperative-cyclic-trowel) <none>
2021-07-20 17:40:23 +0000

    🌏🏦 Checkpoint ./views/maps.ejs:33718558/3779 ./app.js:33718558/254

--- app.js
+++ app.js
@@ -57,13 +57,10 @@ app.get('/locTest', (req,res)=>res.render('locTest'))
 app.get('/contributions', (req,res)=>res.render('contributions'))
 
 
-app.get("/", function(req, res) {
-  res.render("maps.ejs", {
-    place_id: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-    lat: -33.8666199,
-    lng: 151.1958527
-  });
-});
+app.get('/map',
+  isLoggedIn,
+  (req,res) => {res.render('map')
+})
 
 
 
